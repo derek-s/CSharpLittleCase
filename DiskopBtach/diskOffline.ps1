7fc47953131683f426666ec24e46f29edaaeb9a6
Derek.S <derekseli@outlook.com>
2020-02-28 16:15:25 +0800

    rename

--- DiskopBtach/diskOffline.ps1
+++
@@ -1,8 +0,0 @@
-$diskNum=8
-for($i=0;$i -le $diskNum;$i++){
-    $selectDisk = "select disk x" -replace "x", $i
-    $selectDisk, "offline disk" | diskpart
-}
-
-& "powercfg" -x disk-timeout-ac 1
-& "powercfg" -x disk-timeout-dc 1
--- DiskopBtach/diskReadOnly.ps1
+++
@@ -1,5 +0,0 @@
-$diskNum=8
-for($i=0;$i -le $diskNum;$i++){
-    $selectDisk = "select disk x" -replace "x", $i
-    $selectDisk, "att disk clear readonly" | diskpart
-}
