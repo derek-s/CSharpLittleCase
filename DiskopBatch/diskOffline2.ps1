cf12f593bff4a41112b661c562958360cba7b602
Derek.S <derekseli@outlook.com>
2020-02-28 16:15:35 +0800

    update

---
+++ DiskopBatch/diskOffline2.ps1
@@ -0,0 +1,33 @@
+﻿# 批量硬盘脱机 跳过指定硬盘
+
+# 硬盘数量
+$diskNum=8
+
+# 硬盘序列号
+$diskSN = "WD-WCC4M7XFSU0P"
+# 正则表达式
+$pattern = "\d"
+
+# 遍历本机全部硬盘的信息
+$diskInfo = Get-WmiObject Win32_PhysicalMedia
+foreach($n in $diskInfo){
+#$n["SerialNumber"]
+if($n["SerialNumber"].Contains($diskSN)){
+    $diskTagString = $n["Tag"]
+    }
+}
+
+# 正则匹配获得磁盘号
+$diskTagString -match $pattern
+$diskTagNumber = [Int32]($Matches.Values | Out-String)
+
+$diskTagNumber.GetType().Name
+
+
+# 操作diskpart
+for($i=0;$i -le $diskNum;$i++){
+    if($i -ne $diskTagNumber){
+        $selectDisk = "select disk x" -replace "x", $i
+        $selectDisk, "offline disk" | diskpart
+        }
+}
